28bbec187f36863d2af87a9933c6094feadb2194
Christian Schmidt <developGames@gmx.de>
2022-11-23 21:36:47 +0100

    js tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if (typeof(count) != "number" || count < 0) {
+        return "ERROR";
+    }
+    let buffer = "";
+    for (i = 0; i < count; ++i) {
+        buffer += string;
+    }
+    return buffer;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...valuesToRemove) {
+    for (let i = 0; i < valuesToRemove.length; ++i) {
+        array = array.filter(function(value, i, arr) { return value !== valuesToRemove[i];});
+    }
+    return array;
 };
 
 // Do not edit below this line
